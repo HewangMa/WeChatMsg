ad31e2a301f464b668ad6504ca9f2084c94c2faa
mahewang <xzz14939@gmail.com>
2025-03-05 11:38:32 +0800

    分段waken逻辑

--- app/waken/waken.py
+++ app/waken/waken.py
@@ -21,9 +21,6 @@ class Waken:
             if one_msg_.startswith('<') or len(one_msg_) < 1:
                 continue
             ret.append(f"{sender}: {one_msg_}")
-        for i in range(len(ret)-1, -1, -1):
-            if i > 0 and ret[i][0] == ret[i-1][0]:
-                ret[i] = "    " + ret[i][3:]
         return ret
 
     def __waken_one_para(self, para):
@@ -31,7 +28,7 @@ class Waken:
             msg = file.read()
         msg = msg.replace("{records}", para)
         response = Conversation().get_response_from_llm(question=msg)
-        self.__exact_and_write(response, YEAR, MONTH, DAY)
+        return response
 
     def waken_one_day(self, YEAR, MONTH, DAY):
         start_time = f"{str(YEAR)}-{str(MONTH)}-{str(DAY)} {HOUR_TIME}"
@@ -45,23 +42,32 @@ class Waken:
         # 根据总量划分waken次数，并分段（前后来个bias）进行
         whole_msg_in_one_day = "\n".join(msg_in_one_day)
         token_len = token_len_of(whole_msg_in_one_day)
-        part_num = token_len//2000
-        part_msg_cnt = msg_cnt//part_num
+        part_num = token_len // 2000 + 1
+        part_msg_cnt = msg_cnt // part_num
+        
         for p_idx in range(part_num):
-            start = p_idx*part_msg_cnt
-            if start > 0:
-                start -= self.bias
-            end = (p_idx+1)*part_msg_cnt
-            if end < msg_cnt-1:
-                end += self.bias
-            self.__waken_one_para("\n".join(msg_in_one_day[start:end]))
+            start_idx = p_idx*part_msg_cnt
+            if start_idx > 0:
+                start_idx -= self.bias
+            end_idx = (p_idx+1)*part_msg_cnt
+            if end_idx < msg_cnt-1:
+                end_idx += self.bias
+            logger.info(f"Trying with start = {start_idx}, end = {end_idx}, msg_cnt = {msg_cnt}")
 
+            para = msg_in_one_day[start_idx:end_idx+1]
+            # 分出段之后 去除重复sender
+            for i in range(len(para)-1, -1, -1):
+                if i > 0 and para[i][0] == para[i-1][0]:
+                    para[i] = "    " + para[i][3:]
+            logger.info('\n'.join(para))
+            one_waken = self.__waken_one_para("\n".join(para))
+            logger.info(one_waken)
+            break
 
     def __exact_and_write(self, response, YEAR, MONTH, DAY):
         header = f"{str(YEAR)}-{str(MONTH)}-{str(DAY)}: "
         paras = response.split("{{split}}")
-        to_write = ['\n', header]+[self.beauty +
-                                   '\n' + p for p in paras if len(p) > 2]
+        to_write = ['\n', header]+[self.beauty +'\n' + p for p in paras if len(p) > 2]
         file_name = f"{str(YEAR)}-{str(MONTH)}.txt"
         file_path = os.path.join(WAKEN_RES_ROOT, file_name)
         with open(file_path, 'a', encoding='utf-8') as file:
@@ -72,8 +78,8 @@ class Waken:
 if __name__ == "__main__":
     day_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     YEAR = 2023
-    MONTH = 3
-    # DAY = 1
-    # Waken().waken_one_day(YEAR, MONTH, DAY)
-    for DAY in range(1, day_in_month[MONTH-1]+1):
-        Waken().waken_one_day(YEAR, MONTH, DAY)
+    MONTH = 2
+    DAY = 1
+    Waken().waken_one_day(YEAR, MONTH, DAY)
+    # for DAY in range(1, day_in_month[MONTH-1]+1):
+    #     Waken().waken_one_day(YEAR, MONTH, DAY)
